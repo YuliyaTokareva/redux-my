7c161fde624b163f36de9c15faa3cf25c937f90d
Yuliya Tokareva <yuliya@byzantium.com.ua>
2022-08-09 17:14:54 +0300

    add

--- lesson2/task1/src/index.js
+++ lesson2/task1/src/index.js
@@ -1,22 +1,22 @@
 import { createStore } from 'redux'
-// const INCREMENT = 'COUNTER/INCREMENT'
-// const DECREMENT = 'COUNTER/DECREMENT'
+const INCREMENT = 'COUNTER/INCREMENT'
+const DECREMENT = 'COUNTER/DECREMENT'
 export const increment = () => {
     return {
-        type: 'COUNTER/INCREMENT',
+        type: INCREMENT,
     }
 }
 export const decrement = () => {
     return {
-        type: 'COUNTER/DECREMENT',
+        type: DECREMENT,
     }
 }
 
 const counterReduser = (state = 0, action) => {
     switch (action.type) {
-        case 'COUNTER/INCREMENT':
+        case INCREMENT:
             return state + 1
-        case 'COUNTER/DECREMENT':
+        case DECREMENT:
             return state - 1
         default:
             return state
