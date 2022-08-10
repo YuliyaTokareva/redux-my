db09627d481ed589a39116d59d50960ffde0456f
Yuliya Tokareva <yuliya@byzantium.com.ua>
2022-08-10 21:40:45 +0300

    add

--- lesson3/task1/src/counter.reducer.js
+++ lesson3/task1/src/counter.reducer.js
@@ -1,24 +1,18 @@
 import { INCREMENT, DECREMENT, RESET } from './counter.actions'
-const initialState = {
-    counter: 0,
-}
-export const counterReduser = (state = initialState, action) => {
+// const initialState = {
+//     counter: 0,
+// }
+export const counterReduser = (state = 0, action) => {
     switch (action.type) {
         case INCREMENT:
-            return {
-                ...state,
-                counter: initialState.counter + 1,
-            }
+            return state + 1
+
         case DECREMENT:
-            return {
-                ...state,
-                counter: initialState.counter - 1,
-            }
+            return state - 1
+
         case RESET:
-            return {
-                ...state,
-                counter: 0,
-            }
+            return 0
+
         default:
             return state
     }
--- lesson3/task1/src/index.js
+++ lesson3/task1/src/index.js
@@ -4,6 +4,8 @@ import { addUser, deleteUser, updareUser } from './users.actions'
 store.subscribe(() => console.log(store.getState()))
 store.dispatch(increment())
 
+store.dispatch(decrement())
+
 store.dispatch(addUser({ id: 76, name: 'Sarah' }))
 
 store.dispatch(deleteUser(76))
