9a20471186d1fb938b3be912b7814617a8b308c4
Yuliya Tokareva <yuliya@byzantium.com.ua>
2022-08-11 20:35:28 +0300

    add

--- lesson4/task3/src/Pagination.jsx
+++ lesson4/task3/src/Pagination.jsx
@@ -7,7 +7,7 @@ class Pagination extends Component {
     render() {
         const { users, currentPage, nextPage, prevPage } = this.props
         const totalItems = Math.ceil(users.length / 3)
-        const isPrevPageAvailable = currentPage + 1 === totalItems
+        const isPrevPageAvailable = currentPage === totalItems
         const isNextPageAvailable = currentPage === 0
 
         return (
@@ -16,7 +16,7 @@ class Pagination extends Component {
                     <button className="btn" onClick={prevPage} disabled={isNextPageAvailable}>
                         {isNextPageAvailable ? '' : '←'}
                     </button>
-                    <span className="pagination__page">{currentPage + 1}</span>
+                    <span className="pagination__page">{currentPage}</span>
                     <button className="btn" onClick={nextPage} disabled={isPrevPageAvailable}>
                         {currentPage === totalItems - 1 ? '' : '→'}
                     </button>
@@ -29,7 +29,7 @@ class Pagination extends Component {
 const mapState = (state) => {
     return {
         users: state.users.usersList,
-        currentPage: state.users.currentPage,
+        currentPage: state.users.currentPage + 1,
     }
 }
 const mapDispatch = {
